5ae156877db8ef74fc68f914d6fd4315a008c99e
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-04-17 10:49:06 +0530

    dsa

--- practice.py
+++ practice.py
@@ -27,3 +27,29 @@ added_numbers = list(map(lambda x,y:x+y, numbers1, numbers2))
 print(added_numbers)
 
 print()
+
+
+'''
+Count consonants in a string
+Problem Description:
+
+You are given a string s. Your task is to count the number of consonants in the string and return the total count. A consonant is any alphabetic character that is not a vowel (a, e, i, o, u).
+
+
+Input:
+
+A single string s, where the length of s is between 1 and 1000.
+
+Output:
+
+An integer representing the total count of consonants in the input string.
+
+
+Example:
+
+Input: "Hello, World!"
+Output: 7
+ 
+Input: "Python Programming"
+Output: 13
+'''
