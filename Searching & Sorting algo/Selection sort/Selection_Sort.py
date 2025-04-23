d9056912dd28903acd84412b925fa58b8689dcf6
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-04-23 18:24:25 +0530

    Selection sort learning

---
+++ Searching & Sorting algo/Selection sort/Selection_Sort.py
@@ -0,0 +1,10 @@
+'''
+You are given a list of integers. Write a Python function to sort the list in ascending order using the Selection Sort algorithm. Selection Sort works by repeatedly finding the minimum element from the unsorted part of the list and swapping it with the first element of the unsorted part.
+
+Example:
+Input: lst = [64, 25, 12, 22, 11]
+Output: [11, 12, 22, 25, 64]
+
+Input: lst = [29, 10, 14, 37, 13]
+Output: [10, 13, 14, 29, 37]
+'''
