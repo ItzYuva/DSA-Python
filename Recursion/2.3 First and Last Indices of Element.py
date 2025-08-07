3a9c7783dee40306790b7713a42c2d5b803a20b0
Aditya Kumar Sinha <adityauv11@gmail.com>
2025-08-07 09:01:38 +0530

    first & Last index using recursion

--- Recursion/2.3 First and Last Indices of Element.py
+++ Recursion/2.3 First and Last Indices of Element.py
@@ -20,7 +20,14 @@ def lastIndexOfAnElement(l1,x):
     if(len(l1)==0):
         return -1
     
+    ansFromRecursion = lastIndexOfAnElement(l1[1:], x)
+
+    if ansFromRecursion != -1:
+        return ansFromRecursion +1
+    elif l1[0] == x:
+        return 0
+    else:
+        return -1
 
 print(firstIndexOfAnElement([3,2,5,2,8,2,1],2))
-print(firstIndexOfAnElement([3,2,5,2,8,2,1],10))
-print(firstIndexOfAnElement([3,2,5,2,8,2,1],1))
+print(lastIndexOfAnElement([3,2,5,2,8,2,1],2))
