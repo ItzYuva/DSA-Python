a19c2f4049edd8e115efc6766fd3753fed0a9290
ItzYuva <adityauv11@gmail.com>
2026-05-17 17:47:57 +0530

    decimal to binary

---
+++ Bit Manipullation/decimal2binary.py
@@ -0,0 +1,16 @@
+# write the code to convert decimal to binary
+
+def decimal2binary(num:int)->str:
+    result = ""
+    while num>0:
+        if num % 2 ==0:
+            result += "1"
+        else:
+            result += "0"
+        num = num//2
+    return result[::1]
+
+#  time complexity will be O(log n) where n is the input number
+#  space complexity will be O(log n) where n is the input number
+#  How space complexity is O(log n) ?
+#  The space complexity is O(log n) because the number of bits required to represent a decimal number in binary is proportional to the logarithm of the number. For example, a decimal number n can be represented in binary using log2(n) bits. Therefore, the space required to store the binary representation of the number grows logarithmically with respect to the input number.
