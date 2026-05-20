f0d423088e2dd15cd16eb0e1aa54e85b6931eeaf
ItzYuva <adityauv11@gmail.com>
2026-05-20 09:54:09 +0530

    Swap numbers using XOR

--- Bit Manipullation/SwapNumbers.py
+++ Bit Manipullation/SwapNumbers.py
@@ -1,2 +1,15 @@
 # Write the code to swap numbers using bit manipulation (XOR operator)
 
+def swap_numbers(a, b):
+    a = a ^ b
+    b = a ^ b
+    a = a ^ b
+    return a, b
+
+a = 3
+b = 5
+print(f"Before swapping: a = {a}, b = {b}")
+a, b = swap_numbers(a, b)
+print(f"After swapping: a = {a}, b = {b}")
+#  time complexity will be O(1) because we are performing a constant number of operations regardless of the input size.
+#  space complexity will be O(1) because we are using a constant amount of space
