0a820d700e85e11cc02f0955bd1a7deb9926b1c6
ItzYuva <adityauv11@gmail.com>
2026-05-30 13:55:46 +0530

    easy ques

---
+++ Recursion/Reverse sub array.py
@@ -0,0 +1,15 @@
+'''
+Given an array arr, you need to reverse a subarray of that array. The range of this subarray is given by indices l and r (1-based indexing).
+
+Example 1:
+
+Input: arr[] = [1, 2, 3, 4, 5, 6, 7], l = 2, r = 4
+Output: [1, 4, 3, 2, 5, 6, 7]
+Explanation: After reversing the elements in range 2 to 4 (2, 3, 4), modified array is 1, 4, 3, 2, 5, 6, 7.
+
+Example 2:
+
+Input: arr[] = [1, 6, 7, 4], l = 1, r = 4
+Output: [4, 7, 6, 1]
+Explanation: After reversing the elements in range 1 to 4 (1, 6, 7, 4), modified array is 4, 7, 6, 1.
+'''
